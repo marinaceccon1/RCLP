3c8810dc434f62cfe7a6a215d0d8c16132060281
marinaceccon1 <107349467+marinaceccon1@users.noreply.github.com>
2025-02-12 16:47:37 +0100

    Update Training-Replay.py

--- src/Training-Replay.py
+++ src/Training-Replay.py
@@ -1,19 +1,15 @@
 
 import sys
 import os
-base_path = #insert the path where the repository was imported
+current_path = os.path.abspath(__file__)
+base_path = os.path.dirname(os.path.dirname(current_path))
 sys.path.append(base_path)
 
-from sklearn.metrics import roc_auc_score
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import transforms
-import numpy as np
-from torch.utils.data import Dataset, DataLoader, Subset
-import matplotlib.pyplot as plt
-from src.data.dataset import NIH
-from src.data.dataset import CheXpert
+from torch.utils.data import DataLoader
 from src.data.utils import import_nih_dfs
 from src.data.utils import import_cxp_dfs
 from src.data.utils import create_datasets
@@ -21,14 +17,9 @@ from src.data.utils import create_datasets_joint_cxp
 from src.data.utils import create_datasets_joint_nih
 from src.data.utils import create_dataloaders
 from src.data.utils import create_buffer
-from sklearn.metrics import roc_auc_score
-from src.data.utils import MergedDataset
-from src.data.utils import filter_target
 from src.training.utils import train_model_replay
 from src.eval.utils import eval_model
 from src.eval.utils import compute_auc_and_f1
-import time
-import os
 
 #define the gpu device used to attach the tensors
 device = torch.device('cuda:2')
